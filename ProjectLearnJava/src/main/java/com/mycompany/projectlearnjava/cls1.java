320b27c551bf89315e0eb29a856aa4dff93df624
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-07-11 04:54:19 +0200

    edit 11072020

--- ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/cls1.java
+++ ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/cls1.java
@@ -10,5 +10,11 @@ package com.mycompany.projectlearnjava;
  * @author DevGlee
  */
 public class cls1 {
+        public static void main(String [] args) {
+            // TODO Auto-generated method stub 
+            
+            System.out.println("Hello World!");
+            System.out.println("My name is arnaud");
+        }
     
 }
