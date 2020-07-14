ddee2a5d6593fda2397c5da8acd60cee87322663
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-07-14 02:49:08 +0200

    Update cls1.java

--- ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/cls1.java
+++ ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/cls1.java
@@ -65,6 +65,22 @@ public class cls1 {
         double nbre1 = 50, nbre2 = 100;
         int resultat = (int) (nbre1 * nbre2);
         System.out.println("Le resultat de l'opération est : " + resultat);
+       
+        
+        // Conversion , cast
+        int nb1 = 8 , nb2 =7;
+        double resultat1 =  ((double) nb1 / (double)nb2); // Cast de chaque nombre avec le bon type de variable pour avoir le résulta correct
+        System.out.println("Le resultat est" + resultat1);
+        
+        
+        //Ici la variable u est égale a 42
+        int u =42;
+        String z = new String();//variable z de type string
+        z = z.valueOf(u);// String z hérite du nombre 42 
+        int s = Integer.valueOf(z).intValue();// int s prend la valeur de z
+        System.out.println("s = " + s);//La variable s de type int est égale à 42
+        
+        
         }
     
 }
