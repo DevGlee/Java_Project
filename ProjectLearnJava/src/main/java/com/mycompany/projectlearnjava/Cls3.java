5642ae2733ef821d448e98a905695e9b3dce7aae
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-07-16 07:23:57 +0200

    Update Cls3.java

--- ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/Cls3.java
+++ ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/Cls3.java
@@ -15,6 +15,7 @@ public class Cls3 {
 
     public static void main(String[] args) {
 
+        //Conditions if
         int i = -10;
 
         if (i > 0) {
@@ -26,15 +27,46 @@ public class Cls3 {
             System.out.println("L'entrée est nulle");
 
         }
-       
-        
-        int j=100;
-        
-        if(j>=0 && j<=100){
+
+        int j = 100;
+
+        if (j >= 0 && j <= 100) {
             System.out.println("Le nombre est compris dans l'intervalle");
-        }
-        else {
+        } else {
             System.out.println("Le nombre n'est pas compris dans l'intervalle");
+        }
+
+        //Switch int
+        int note = 20; //Note sur 20
+
+        switch (note) {
+            case 0:
+                System.out.println("Aucun travail");
+                break;
+
+            case 10:
+                System.out.println("Tout juste la moyenne");
+                break;
+            case 20:
+                System.out.println("Excellent Travail");
+                break;
+            default:
+                System.out.println("Il faut davantage travailler");
+        }
+        
+        //Switch string
+        String chaine = "Bonjour" ;
+
+        switch (chaine) {
+            case "Bonjour":
+                System.out.println("Bonjour monsieur");
+                break;
+
+            case "Bonsoir":
+                System.out.println("Bonsoir monsieur");
+                break;
+            default:
+                System.out.println("Bonjoir :V");
         }
     }
 
