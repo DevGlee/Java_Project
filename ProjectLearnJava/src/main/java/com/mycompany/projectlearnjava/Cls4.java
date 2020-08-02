feab245c798d7028d565b138ec2f2a8710e474b3
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-08-02 21:27:50 +0200

    Create Cls4.java

---
+++ ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/Cls4.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectlearnjava;
+
+/**
+ *
+ * @author DevGlee
+ */
+
+// LES TABLEAUX
+
+public class Cls4 {
+    
+    public static void main(String [] args) {
+        
+        char alpha [] =  {'a','b','c','d','e','f','g','i','j','k'};
+        
+        
+       for (int i=0; i<alpha.length; i++)
+       {
+           System.out.println("A l'emplacement " + i + " l'indice = " + alpha[i]);
+       }
+        
+    }
+    
+}
