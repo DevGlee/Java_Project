e5ed8c3180c6fa938664fa9f6ee2603c4335ff53
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-10-07 17:24:12 -0400

    update

---
+++ ProjectLearnJava/src/main/java/com/mycompany/projectlearnjava/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectlearnjava;
+
+/**
+ *
+ * @author DevGlee
+ */
+public class Player {
+    
+    private String name="Drill";
+    private double health=20;
+    private double attack=0.5;
+    
+    public Player() {
+        
+    }
+    
+}
