cc9a66df60ce6c82d38b8c9a74c7b6878ed22dfd
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-10-19 16:11:24 -0400

    update

---
+++ ProjectLearnJava/src/main/java/fr/DevGlee/programme/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.DevGlee.programme;
+
+/**
+ *
+ * @author DevGlee
+ */
+// Classe Principale
+public class Main {
+
+    public static void main(String[] args) {
+
+        int money = 850;
+        int pricephone = 800;
+        boolean hasPhone = false;
+
+        if (money >= pricephone && !hasPhone) {
+
+            System.out.println("Tu peux acheter le telephone");
+        } else {
+            System.out.println("Tu ne peux pas acheter le telephone ou tu en as déjà un ");
+        }
+
+      String pseudoss ="DevGlee,Bijoul,Toby";
+      String[] pseudos= pseudoss.split(",");
+      System.out.println(pseudos[1]);
+
+        
+    }
+}
