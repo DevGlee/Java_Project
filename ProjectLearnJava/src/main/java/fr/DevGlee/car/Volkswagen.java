ab7fcfe2c0542405f859c28c6d509796627dc5ea
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-10-30 13:07:14 -0400

    update

--- ProjectLearnJava/src/main/java/fr/DevGlee/car/Volkswagen.java
+++ ProjectLearnJava/src/main/java/fr/DevGlee/car/Volkswagen.java
@@ -9,6 +9,25 @@ package fr.DevGlee.car;
  *
  * @author DevGlee
  */
-public class Volkswagen {
+public class Volkswagen extends Car implements Feature {
+
+    public Volkswagen(String model, double weight, double reservoir, int horsepower, String fuel) {
+        super(model, weight, reservoir, horsepower, fuel);
+    }
+
+    @Override
+    public String Engine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double Speed() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double Acceleration() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
